6d81e82d289b1e64bb642f44bdc4506f2eef5f70
Achiengy <hope.pete@student.moringaschool.com>
2022-03-23 12:26:13 +0300

    Add event listener for the image loading function

---
+++ JS/profile.js
@@ -0,0 +1,14 @@
+
+
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+            }
+  
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+        }
+    });
+  });
